b945fdd006396355e873c3018a4d735086892b84
0xSam <smorahschool@gmail.com>
2023-10-05 11:10:54 +0100

    initialized a next 13 project

---
+++ airbnb/app/layout.tsx
@@ -0,0 +1,28 @@
+import { Nunito } from 'next/font/google';
+
+import './globals.css';
+import Navbar from "./components/navbar/Navbar";
+
+import type { Metadata } from 'next'
+
+const font = Nunito({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar />
+        {children}
+        </body>
+    </html>
+  )
+}
