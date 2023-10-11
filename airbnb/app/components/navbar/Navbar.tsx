3e3b43a1562a75ecc31c555d496c1d008b67c4ff
0xSam <smorahschool@gmail.com>
2023-10-11 13:16:52 +0100

    Categories UI

--- airbnb/app/components/navbar/Navbar.tsx
+++ airbnb/app/components/navbar/Navbar.tsx
@@ -7,6 +7,7 @@ import Logo from "./Logo";
 import Search from "./Search"
 import UserMenu from "./UserMenu";
 import { SafeUser } from "@/app/types";
+import Categories from "./Categories";
 
 interface NavbarProps {
     currentUser?: SafeUser | null;
@@ -39,6 +40,7 @@ const Navbar: React.FC<NavbarProps> = ({
                     </div>
                 </Container>
             </div>
+            <Categories />
         </div>
     )
 }
